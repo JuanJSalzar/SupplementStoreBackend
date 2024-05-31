c8b209825f1a8bc5fc1e15574ed04eca1b25e5f8
juanjose12tj <78375347+juanjose12tj@users.noreply.github.com>
2024-05-31 08:29:42 -0500

    Updated the messages in Helpers folder
    This commit was required by the TL for documentation

--- Helpers/Messages.cs
+++ Helpers/Messages.cs
@@ -1,5 +1,6 @@
 public static class Messages
 {
+    /* This messages are will help each person creating a controller */
     public static string ProductNotFound = "Product not found";
     public static string ProductCreated = "Product created successfully";
     public static string ProductUpdated = "Product updated successfully";
